5fbf0cbc9725e32e93b9dd6d9c38cd251518d9fb
xostrch <xostrch@icloud.com>
2025-06-15 17:24:08 +0200

    i added full responsive styles in css

--- main.js
+++ main.js
@@ -29,19 +29,19 @@ let scrollPos = 0;
 let isScrolling = false;
 
 function step() {
-  if (isScrolling) {
-    scrollPos += 2;
+  scrollPos += 1;
 
-    if (scrollPos >= track.scrollWidth / 2) {
-      scrollPos = 0;
-    }
-
-    container.scrollLeft = scrollPos;
+  if (scrollPos >= track.scrollWidth / 2) {
+    scrollPos = 0;
   }
+
+  container.scrollLeft = scrollPos;
+
   requestAnimationFrame(step);
 }
 
 step();
+
 
 container.addEventListener('mouseenter', () => {
   isScrolling = true;
